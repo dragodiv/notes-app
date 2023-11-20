a5daeda9a3220548598003ec781a091d0e200133
dragodiv <dragodiv18@gmail.com>
2023-11-20 15:49:25 +0530

    update

--- src/App.jsx
+++ src/App.jsx
@@ -8,8 +8,8 @@ import './App.css';
 import { notescollection , db } from "./Firebase.jsx"
 
 export default function App() {
-  const [notes, setNotes] = useState(JSON.parse(localStorage.getItem("notes")) || [])
-  // const [notes, setNotes] = useState([])
+  // const [notes, setNotes] = useState(JSON.parse(localStorage.getItem("notes")) || [])
+  const [notes, setNotes] = useState([])
   const [currentNoteId, setCurrentNoteId] = useState(
     (notes[0] && notes[0].id) || ""
   )
